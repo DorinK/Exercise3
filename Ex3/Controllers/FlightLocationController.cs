a052e715703e7a04f2c9418ff0b6ffc5b83e2639
Dorin Keshales <dorin140895@gmail.com>
2019-05-27 15:28:17 +0300

    Creating controller and it's view.

---
+++ Ex3/Controllers/FlightLocationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ex3.Controllers
+{
+    public class FlightLocationController : Controller
+    {
+        // GET: FlightLocation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
